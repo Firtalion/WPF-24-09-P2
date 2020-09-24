cecc569ff605e2d71c42fff2133d52c29f9799ec
Firtalion <oliviermael@gmail.com>
2020-09-24 12:33:38 +0200

    Ajoutez des fichiers projet.

---
+++ WPF 24-09 P2/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_24_09_P2
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            client client = new client();
+            client.Nom = "Olivier";
+            client.Prénom = "Mael";
+            client.Adresse = "18 rue bidon";
+            client.Localite = "ville bidon";
+            client.Telephone = 45296426;
+
+        }
+        public class client
+        {
+            public int Id { get; set; }
+            public string Nom { get; set; }
+            public string Prénom { get; set; }
+            public string Adresse { get; set; }
+            public string Localite { get; set; }
+            public int Telephone { get; set; }
+
+
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+
+
+        }
+    }
+}
